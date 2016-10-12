f5665d39f05b5b511dc2d8f9522a23ef97490674
dc <dc@a2consulting.com.au>
2016-10-12 13:07:29 +1100

    DEV-29822 DAdd copyright notice

--- app/code/IPGPAY/Gateway/Model/IPGPAY.php
+++ app/code/IPGPAY/Gateway/Model/IPGPAY.php
@@ -1,8 +1,13 @@
 <?php
 /**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
+ * @version $Id$
+ * @copyright Copyright (c) 2002 - 2016 IPG Holdings Limited (a company incorporated in Cyprus).
+ * All rights reserved. Use is strictly subject to licence terms & conditions.
+ * This computer software programme is protected by copyright law and international treaties.
+ * Unauthorised reproduction, reverse engineering or distribution of the programme, or any part of it, may
+ * result in severe civil and criminal penalties and will be prosecuted to the maximum extent permissible at law.
+ * For further information, please contact the copyright owner by email copyright@ipgholdings.net
+ **/
 namespace IPGPAY\Gateway\Model;
 
 use IPGPAY\Gateway\Api\Request\Credit;
