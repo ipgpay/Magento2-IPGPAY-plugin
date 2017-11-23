ab2911bbe12432b719ab49b0e220ee823c72ab14
Wesley <tiankai@shinetechsoftware.com>
2017-11-23 16:13:39 +0800

    code format

--- app/code/IPGPAY/Test/Unit/Controller/Land/CancelTest.php
+++ app/code/IPGPAY/Test/Unit/Controller/Land/CancelTest.php
@@ -7,7 +7,6 @@ use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 class CancelTest extends \PHPUnit\Framework\TestCase
 {
 
-         
     /**
      * @var ObjectManager
      */
@@ -20,7 +19,7 @@ class CancelTest extends \PHPUnit\Framework\TestCase
     protected $orderMock;
 
     protected $cancelObjectManger;
-    
+
     protected $redirectMock;
 
     protected function setUp()
@@ -28,70 +27,67 @@ class CancelTest extends \PHPUnit\Framework\TestCase
         $objectManager = new ObjectManager($this);
 
         $this->checkoutSessionMock = $this
-        ->getMockBuilder(\Magento\Checkout\Model\Session::class)
-        ->disableOriginalConstructor()
-        ->getMock();
+            ->getMockBuilder(\Magento\Checkout\Model\Session::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $this->orderMock = $this-> getMockBuilder(\Magento\Sales\Model\Order::class)
-        ->disableOriginalConstructor()
-        ->getMock();
+        $this->orderMock = $this->getMockBuilder(\Magento\Sales\Model\Order::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $this->checkoutSessionMock ->expects(static::once())
-        ->method('getLastRealOrder')
-        ->willReturn($this->orderMock);
-        ;
+        $this->checkoutSessionMock->expects(static::once())
+            ->method('getLastRealOrder')
+            ->willReturn($this->orderMock);
 
-        $this->checkoutSessionMock ->expects(static::once())
-        ->method('restoreQuote')
-        ->willReturn(false);
+        $this->checkoutSessionMock->expects(static::once())
+            ->method('restoreQuote')
+            ->willReturn(false);
 
-        $this->cancelObjectManger = $this-> getMockBuilder(\Magento\Framework\ObjectManagerInterface::class)
-        ->disableOriginalConstructor()
-        ->getMock();
+        $this->cancelObjectManger = $this->getMockBuilder(\Magento\Framework\ObjectManagerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $this->cancelObjectManger ->expects(static::any())
-        ->method('get')
-        ->with('Magento\Checkout\Model\Session')
-        ->willReturn($this->checkoutSessionMock);
-        
+        $this->cancelObjectManger->expects(static::any())
+            ->method('get')
+            ->with('Magento\Checkout\Model\Session')
+            ->willReturn($this->checkoutSessionMock);
+
         $this->redirectMock = $this->createMock(\Magento\Framework\App\Response\RedirectInterface::class);
 
-        $this->cancelObjectManger ->expects(static::any())
-        ->method('get')
-        ->with('Magento\Checkout\Model\Session')
-        ->willReturn($this->checkoutSessionMock);
-        
+        $this->cancelObjectManger->expects(static::any())
+            ->method('get')
+            ->with('Magento\Checkout\Model\Session')
+            ->willReturn($this->checkoutSessionMock);
 
         $this->controller = $objectManager->getObject(Cancel::class, [
             '_objectManager' => $this->cancelObjectManger,
-            '_redirect' => $this->redirectMock
-            ]);
+            '_redirect'      => $this->redirectMock,
+        ]);
     }
- 
+
     /**
      * @test
      */
     public function testExecute()
     {
-        $this->orderMock ->expects(static::once())
-        ->method('getRealOrderId')
-        ->willReturn(1);
-        ;
+        $this->orderMock->expects(static::once())
+            ->method('getRealOrderId')
+            ->willReturn(1);
 
-        $this->orderMock ->expects(static::once())
-        ->method('cancel')
-        ->willReturnSelf();
-        
-        $this->orderMock ->expects(static::once())
-        ->method('addStatusToHistory')
-        ->willReturnSelf();
+        $this->orderMock->expects(static::once())
+            ->method('cancel')
+            ->willReturnSelf();
 
-        $redirectResponse =  $this->createMock(\Magento\Framework\App\ResponseInterface::class);
-        
-        $this->redirectMock ->expects($this->once())
-        ->method('redirect')
-        ->with($redirectResponse, $this->equalTo('checkout'));
-        
+        $this->orderMock->expects(static::once())
+            ->method('addStatusToHistory')
+            ->willReturnSelf();
+
+        $redirectResponse = $this->createMock(\Magento\Framework\App\ResponseInterface::class);
+
+        $this->redirectMock->expects($this->once())
+            ->method('redirect')
+            ->with($redirectResponse, $this->equalTo('checkout'));
+
         $this->controller->execute();
     }
 }
