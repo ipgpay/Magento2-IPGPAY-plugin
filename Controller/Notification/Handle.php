4870df2d0f0e81caabc5ee6a8e3d928c726f2106
colint <ct@ipggroup.com>
2023-06-13 15:58:53 +1000

    DEV-44230 trim

--- Controller/Notification/Handle.php
+++ Controller/Notification/Handle.php
@@ -484,8 +484,8 @@ class Handle extends Action
             $transaction = $this->payment->addTransaction(Payment\Transaction::TYPE_VOID);
             $transaction->setAdditionalInformation(Payment\Transaction::RAW_DETAILS, $this->fields)->save();
         }
-        $failureReason = ($this->fields['response_code'] ?? '') . ' ' . ($this->fields['response_text'] ?? '');
-        $history = $this->order->addStatusHistoryComment(!empty(trim($failureReason)) ? $failureReason : 'Order Failure');
+        $failureReason = trim(($this->fields['response_code'] ?? '') . ' ' . ($this->fields['response_text'] ?? ''));
+        $history = $this->order->addStatusHistoryComment(!empty($failureReason) ? $failureReason : 'Order Failure');
         $history->setIsCustomerNotified(false); // for backwards compatibility
         $this->order->save();
         return $this;
